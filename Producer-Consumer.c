cdbe5f840726c52256b786b6d5dc3ab9964a7bbe
cadenewell <cnewell@zagmail.gonzaga.edu>
2019-11-10 21:09:45 -0800

    Final changes made

--- Producer-Consumer.c
+++ Producer-Consumer.c
@@ -1,6 +1,6 @@
 /**
-Cade Newell
-CPSC 346:02
+ Cade Newell
+ CPSC 346:02
  */
 
 #include "producer-consumer.h"
@@ -12,29 +12,30 @@ CPSC 346:02
 #define TRUE 1
 
 buffer_item buffer[BUFFER_SIZE];
-/*define semaphores and mutex*/
-pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
+pthread_mutex_t mutex;
 
 int insertPointer = 0, removePointer = 0;
 
 void *producer(void *param);
 void *consumer(void *param);
 
+
 int insert_item(buffer_item item)
 {
     /* Acquire Empty Semaphore */
 	sem_wait(&empty);
+	
 	/* Acquire mutex lock to protect buffer */
 	pthread_mutex_lock(&mutex);
-	
-    buffer[insertPointer++] = item;
-    insertPointer = insertPointer % 7;
-    /* Release mutex lock and full semaphore */
-    pthread_mutex_unlock(&mutex);
+	buffer[insertPointer++] = item;
+	insertPointer = insertPointer % 5;
+
+	/* Release mutex lock and full semaphore */
+	pthread_mutex_unlock(&mutex);
     sem_post(&full);
-	
+
 	return 0;
 }
 
@@ -42,23 +43,27 @@ int remove_item(buffer_item *item)
 {
 	/* Acquire Full Semaphore */
 	sem_wait(&full);
-	/* Acquire mutex lock to protect buffer */
-	pthread_mutex_lock(&mutex);
 
-	buffer[insertPointer--] = item;
-	insertPointer = insertPointer % 7;
+	/* Acquire mutex lock to protect buffer */
+    pthread_mutex_lock(&mutex);
+
+	*item = buffer[removePointer];
+	buffer[removePointer++] = -1;
+	removePointer = removePointer % 5;
+
 	/* Release mutex lock and empty semaphore */
 	pthread_mutex_unlock(&mutex);
-	sem_post(&empty);
+    sem_post(&empty);
 
 	return 0;
 }
-
 
 int main(int argc, char *argv[])
 {
 	int sleepTime, producerThreads, consumerThreads;
 	int i, j;
+
+	srand(time(0));
 
 	if(argc != 4)
 	{
@@ -67,29 +72,29 @@ int main(int argc, char *argv[])
 	}
 
 	/*call atoi to get arguments */
-	sleepTime = atoi(argv[1]);
-	producerThreads = atoi(argv[2]);
-	consumerThreads = atoi(argv[3]);
-
-	/* initializing the locks */
-	printf("pthread_mutex_init: %d\n",pthread_mutex_init(&mutex, NULL));
-	sem_init(&empty, 0, 1);
-	sem_init(&full, 0, 0);
+    sleepTime = atoi(argv[1]);
+    producerThreads = atoi(argv[2]);
+    consumerThreads = atoi(argv[3]);
  
 	/* Create the producer and consumer threads */
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0);
 
-	for(i = 0; i < producerThreads; i++){
-		pthread_t tid;
-		pthread_attr_t atr;
-		pthread_attr_init(&atr);
-		pthread_create(&tid, &atr, producer, NULL);
+    for(i = 0; i < producerThreads; i++)
+	{
+        pthread_t tid;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&tid, &attr, producer, NULL);
 	}
 
-	for(j = 0; j < consumerThreads; j++){
-		pthread_t tid;
-		pthread_attr_t atr;
-		pthread_attr_init(&atr);
-		pthread_create(&tid, &atr, consumer, NULL);
+	for(j = 0; j < consumerThreads; j++)
+	{
+        pthread_t tid;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&tid, &attr, consumer, NULL);
 	}
 
 	/* Sleep for user specified time */
@@ -99,37 +104,32 @@ int main(int argc, char *argv[])
 
 void *producer(void *param)
 {
-	buffer_item item;
-	int i;	 
+    buffer_item item;
 
 	while(TRUE)
 	{
-		i = rand() % 7;
-		sleep(i);
-		item = rand();
-
-		if (insert_item(item))
-			fprintf(stderr, "Error");
-		else 
-			printf("Producer produced %d \n", item);
+        sleep(rand() % 5);
+        
+        item = rand();
+        if (insert_item(item))
+            fprintf(stderr, "report error condition, Producer");
+        else
+            printf("producer produced %d\n",item);
 	}
+
 }
 
 void *consumer(void *param)
 {
 	buffer_item item;
-	int i;	
-	 
+
 	while(TRUE)
 	{
-		i = rand() % 7;
-		sleep(i);
-		item = rand();
+        sleep(rand() % 5);
 
-		if (remove_item(item))
-			fprintf(stderr, "Error");
-		else 
-			printf("Consumer consumed %d \n", item);
- 
+        if (remove_item(&item))
+            fprintf(stderr, "report error condition, Consumer");
+        else
+            printf("consumer consumed %d\n",item);
 	}
 }
--- Producer-Consumer.h
+++ Producer-Consumer.h
@@ -1,7 +1,14 @@
- /* buffer.h*/
+ /*
+Cade Newell
+CPSC 346:02
+producer-consumer.h
+*/
 
 typedef int buffer_item;
 #define BUFFER_SIZE 5
 
 int insert_item(buffer_item item);
 int remove_item(buffer_item *item);
+
+void *producer(void *param);
+void *consumer(void *param);
